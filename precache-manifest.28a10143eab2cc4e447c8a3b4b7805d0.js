1405322130ec570f0b3c7f13c9cdacf90f144164
Parthiban <parthiban.n@navoinc.com>
2020-07-28 09:27:24 +0530

    Updates

---
+++ precache-manifest.28a10143eab2cc4e447c8a3b4b7805d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd0d8fdf73640e329d5fb94b88c0b96",
+    "url": "/notification/index.html"
+  },
+  {
+    "revision": "1e8dfd84fc0b0fb5d30b",
+    "url": "/notification/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "be3cc65cd325ae063cef",
+    "url": "/notification/static/css/main.0dbca5a7.chunk.css"
+  },
+  {
+    "revision": "1e8dfd84fc0b0fb5d30b",
+    "url": "/notification/static/js/2.1a52647f.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/notification/static/js/2.1a52647f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be3cc65cd325ae063cef",
+    "url": "/notification/static/js/main.2b7d21c0.chunk.js"
+  },
+  {
+    "revision": "dbfde672d519584e3b79",
+    "url": "/notification/static/js/runtime-main.e03eb78f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/notification/static/media/logo.5d5d9eef.svg"
+  }
+]);
